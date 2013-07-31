14238709bfdfc77d62486e21a13e31e4938faa02
dkehl <dkehl@switch.tv>
2013-07-31 15:38:47 +1000

    Why the hell does this file keep getting lost?

---
+++ File.js
@@ -0,0 +1,27 @@
+//Created in Eclipse
+//Modified outside of eclipse
+//Modified inside of eclipse
+//ANother external modification
+//Third ec modification
+//EC modification 4
+//EXT mod 4
+//MASTER mod
+//Task 1 implementation part 1
+//Task 1 implementation part 1a
+//Task 1 implemnetation part 1b
+//Task 2 implementation part 1
+//Task 3 start
+//Task 3 input
+//Task 4(EXT)
+//Task 4 EXT2
+//Task 4 final implementation(ECL)
+//EXT master mod
+//Task 5 implementation
+//Task 5 part 2
+//Task 5 part 3
+//Task 5 part 4
+//Task 5 part 5a(ECL)
+//Task 5 part 5b(EXT)
+//Task 5 part 5c(EXT)
+//Task 5 part 5d(EXT)
+//Task 5 part 5e(EXT) //Was the better solution
